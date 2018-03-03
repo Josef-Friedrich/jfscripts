1272f578547cd1db50f918fe1e44c67f7541f8f7
Josef Friedrich <josef@friedrich.rocks>
2018-03-03 21:43:46 +0100

    Use single quotes

--- csv_to_yaml.py
+++ csv_to_yaml.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(description='Convert each row of an CSV '
 parser.add_argument('csv', metavar='CSVFILE', help='A CSV file')
 args = parser.parse_args()
 
-in_file  = open(args.csv, "r")
+in_file  = open(args.csv, 'r')
 
 
 def convert_to_yaml(header, line, counter):
