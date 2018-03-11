417b9496a6f70a11fc10eb68dd7bacba3b9a7be0
Josef Friedrich <josef@friedrich.rocks>
2018-03-11 22:28:54 +0100

    New submodule: _utils

---
+++ jfscripts/_utils.py
@@ -0,0 +1,12 @@
+import shutil
+
+
+def check_executables(executables):
+    errors = []
+    for executable in executables:
+        if not shutil.which(executable):
+            errors.append(executable)
+
+    if errors:
+        raise SystemError('Some commands are not installed: ' +
+                          ' '.join(errors))
