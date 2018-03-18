e4f2775960276e114179e38c2a6503d4329129d9
Josef Friedrich <josef@friedrich.rocks>
2018-03-18 16:05:57 +0100

    Extend function: check_executables

--- jfscripts/_utils.py
+++ jfscripts/_utils.py
@@ -4,9 +4,13 @@ import shutil
 def check_executables(*executables):
     errors = []
     for executable in executables:
-        if not shutil.which(executable):
-            errors.append(executable)
+        if isinstance(executable, tuple):
+            if not shutil.which(executable[0]):
+                errors.append('{} ({})'.format(executable[0], executable[1]))
+        else:
+            if not shutil.which(executable):
+                errors.append(executable)
 
     if errors:
         raise SystemError('Some commands are not installed: ' +
-                          ' '.join(errors))
+                          ', '.join(errors))
--- test/test_utils.py
+++ test/test_utils.py
@@ -19,6 +19,28 @@ class TestUnit(unittest.TestCase):
             self.assertEqual(str(error.exception),
                              'Some commands are not installed: lol')
 
+    def test_check_executables_nonexistent_multiple(self):
+        with mock.patch('shutil.which') as mock_which:
+            mock_which.return_value = None
+            with self.assertRaises(SystemError) as error:
+                _utils.check_executables('lol', 'troll')
+
+            self.assertEqual(str(error.exception),
+                             'Some commands are not installed: lol, troll')
+
+    def test_check_executables_nonexistent_multiple_with_description(self):
+        with mock.patch('shutil.which') as mock_which:
+            mock_which.return_value = None
+            with self.assertRaises(SystemError) as error:
+                _utils.check_executables(
+                    ('lol', 'apt install lol'),
+                    'troll',
+                )
+
+            self.assertEqual(str(error.exception),
+                             'Some commands are not installed: lol (apt '
+                             'install lol), troll')
+
 
 if __name__ == '__main__':
     unittest.main()
