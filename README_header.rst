edda7039b377d32886ad76cb48ea001afdf9abf0
Josef Friedrich <josef@friedrich.rocks>
2018-03-22 20:37:00 +0100

    Add --help texts

---
+++ README_header.rst
@@ -0,0 +1,12 @@
+
+.. image:: http://img.shields.io/pypi/v/jfscripts.svg
+    :target: https://pypi.python.org/pypi/jfscripts
+
+.. image:: https://travis-ci.org/Josef-Friedrich/jfscripts.svg?branch=packaging
+    :target: https://travis-ci.org/Josef-Friedrich/jfscripts
+
+*********
+jfscripts
+*********
+
+A collection of my personal Python scripts.
--- _generate_doc.py
+++ _generate_doc.py
@@ -1,10 +1,76 @@
 #! /usr/bin/env python3
 
-from unittest import mock
+import re
+import subprocess
+import os
 
-def setup(**kwargs):
-    return kwargs
+
+def path(*path_segments):
+    return os.path.join(os.getcwd(), *path_segments)
 
 
-with mock.patch('setup'):
-    import setup
+def open_file(*path_segments):
+    file_path = path(*path_segments)
+    open(file_path, 'w').close()
+    return open(file_path, 'a')
+
+def get_commands():
+    setup_py = open('setup.py', 'r')
+    lines = setup_py.read()
+    return re.findall(r' {8,}\'([a-z0-9-]*.py) = jfscripts.[a-z0-9_]*:main\',',
+                      lines)
+
+
+def get_help(command):
+    return subprocess.Popen('{} --help'.format(command), shell=True,
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+
+def heading(text):
+    return '\n{}\n{}\n\n.. code-block:: text\n\n'.format(text, '-' * len(text))
+
+
+def main():
+    commands = get_commands()
+
+    header = open(path('README_header.rst'), 'r')
+    readme = open_file('README.rst')
+    # sphinx = open_file('doc', 'source', 'cli.rst')
+    #
+    # sphinx_header = (
+    #     'Comande line interface\n',
+    #     '======================\n',
+    #     '\n',
+    #     '.. code-block:: text\n',
+    #     '\n',
+    # )
+
+    # for line in sphinx_header:
+    #     sphinx.write(str(line))
+
+    # footer = open(path('README_footer.rst'), 'r')
+
+    for line in header:
+        readme.write(line)
+
+    readme.write('\n')
+    for command in commands:
+        print(command)
+        _help = get_help(command)
+        _help.wait()
+        readme.write(heading(command))
+
+        for line in _help.stdout:
+            indented_line = '    ' + line.decode('utf-8')
+            readme.write(indented_line)
+            #sphinx.write(indented_line)
+
+    # for line in footer:
+    #     readme.write(line)
+
+    readme.close()
+    # sphinx.close()
+
+
+if __name__ == '__main__':
+    main()
