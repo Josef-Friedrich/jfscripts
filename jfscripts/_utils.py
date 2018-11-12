68f145a36547c5101f4642cd6602975ce6f2eda0
Josef Friedrich <josef@friedrich.rocks>
2018-11-12 09:58:46 +0100

    Move class Capturing to _utils

--- jfscripts/_utils.py
+++ jfscripts/_utils.py
@@ -1,7 +1,9 @@
-import shutil
-import subprocess
+from io import StringIO
 from termcolor import colored
 import os
+import shutil
+import subprocess
+import sys
 
 
 class Run(object):
@@ -137,3 +139,30 @@ class FilePath(object):
     def remove(self):
         """Remove the file."""
         os.remove(self.path)
+
+
+class Capturing(list):
+    """see https://stackoverflow.com/a/16571630"""
+
+    def __init__(self, channel='stdout'):
+        self.channel = channel
+
+    def __enter__(self):
+        if self.channel == 'stdout':
+            self._pipe = sys.stdout
+            sys.stdout = self._stringio = StringIO()
+        elif self.channel == 'stderr':
+            self._pipe = sys.stderr
+            sys.stderr = self._stringio = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        del self._stringio
+        if self.channel == 'stdout':
+            sys.stdout = self._pipe
+        elif self.channel == 'stderr':
+            sys.stderr = self._pipe
+
+    def tostring(self):
+        return '\n'.join(self)
--- test/test_utils.py
+++ test/test_utils.py
@@ -130,5 +130,19 @@ class TestClassFilePath(TestCase):
         self.assertTrue(a == b)
 
 
+class TestCapturing(TestCase):
+
+    def test_output_list(self):
+        with _utils.Capturing() as output:
+            print('test')
+        self.assertEqual(output, ['test'])
+
+    def test_method_tostring(self):
+        with _utils.Capturing() as output:
+            print('test1')
+            print('test2')
+        self.assertEqual(output.tostring(), 'test1\ntest2')
+
+
 if __name__ == '__main__':
     unittest.main()
