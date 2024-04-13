8c506f3e49012f2c4a5ffaf20357c6a2c29e8a2e
Josef Friedrich <josef@friedrich.rocks>
2024-04-13 18:32:05 +0200

    Skip tests

--- tests/test_dns_ipv6_prefix.py
+++ tests/test_dns_ipv6_prefix.py
@@ -1,12 +1,16 @@
 import subprocess
 
+import pytest
+
 from tests._helper import is_executable
 
 
 class TestIntegration:
+    @pytest.mark.skip("Not working on GitHub Actions")
     def test_command_line_interface(self) -> None:
         assert is_executable("dns_ipv6_prefix")
 
+    @pytest.mark.skip("Not working on GitHub Actions")
     def test_option_version(self) -> None:
         output = subprocess.check_output(["dns-ipv6-prefix.py", "--version"])
         assert output
